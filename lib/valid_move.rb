9e2b178a3ae823a414b58b6015f63bfccaa2b9ba
Kate Summerill <kaitlynsummerill@gmail.com>
2019-10-25 23:34:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,11 +5,22 @@ def valid_move?(array, index)
   valid_num = index.between?(0, 8)
   if position_taken?(array, index) == false
     return true
-  elsif position_taken?(array, index) == true
-    return false
-  else valid_num  
+  # elsif position_taken?(array, index) == true
+  #   return false
+  # else valid_num  
   end
 end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(array, index)
+  if array[index] == " " || array[index] == "" || array[index] == nil
+    return false
+  elsif array[index] == "X" || array[index] == "O"
+    return true
+  end
+end
+
 
 
 # def valid_move?(array, index)
@@ -23,12 +34,4 @@ end
 # end
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-def position_taken?(array, index)
-  if array[index] == " " || array[index] == "" || array[index] == nil
-    return false
-  elsif array[index] == "X" || array[index] == "O"
-    return true
-  end
-end
