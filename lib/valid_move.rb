5753e2840b75b859ab325d751e066b7c0849f996
Kate Summerill <kaitlynsummerill@gmail.com>
2019-10-25 23:26:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,32 +1,10 @@
-# def display_board(board)
-#   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-#   puts "-----------"
-#   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-#   puts "-----------"
-#   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-# end
-#
-# # code your input_to_index
-#   def input_to_index(input)
-#     # convert input to integer and subtracts 1
-#     input.to_i - 1
-#   end
-#
-# # # code the move method here!
-#   def move(array, index, character = "X")
-#     array[index] = character
-#   end
-
-
 # code your #valid_move? method here
 # return true if the index value is equal to an array 1-9
 # use position_taken method to check a space is not already filled
 def valid_move?(array, index)
   if position_taken?(array, index) == false
     return true
-  elsif position_taken?(array, index) == true
-    return false
-  else valid_move?(array, index).between?(0, 8)
+  else position_taken?(array, index) == true || valid_move?(array, index).between?(0, 8)
     return false
   end
 end
